02f8d082cfc13f2b21c941bf40666b9f5a68d658
Priyal2807 <priyalsobti@gmail.com>
2018-01-19 22:17:46 +0530

    Javascript 30 Wesbos

--- sort.js
+++ sort.js
@@ -1,1 +1,10 @@
 const Benedict = ['The Abdominable Bride','The Child In Time','Doctor Strange','Twelve years a slave','Starter for 10','The Fifth estate','Star Trek Into Darkness','The Imitation Game','Sherlock','The Hollow Crown','Atonement','The Cuurent War'];
+const sortbenedict = Benedict.sort(function(a,b){
+  if(a>b){
+    return 1;
+  }
+  else {
+    return -1;
+  }
+})
+console.log(sortbenedict);
