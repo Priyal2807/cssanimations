a058959fd2b20f36e2e2472d73540e01886e000b
Priyal2807 <priyalsobti@gmail.com>
2018-01-19 22:50:21 +0530

    Javascript 30 Wesbos

--- sort.js
+++ sort.js
@@ -1,10 +1,19 @@
-const Benedict = ['The Abdominable Bride','The Child In Time','Doctor Strange','Twelve years a slave','Starter for 10','The Fifth estate','Star Trek Into Darkness','The Imitation Game','Sherlock','The Hollow Crown','Atonement','The Cuurent War'];
-const sortbenedict = Benedict.sort(function(a,b){
-  if(a>b){
+const Benedict = ['The Abdominable Bride','The Child In Time','Doctor Strange','Twelve years a slave','Starter for 10','The Fifth estate','Star Trek Into Darkness','The Imitation Game','Sherlock','The Hollow Crown','Atonement','The Current War'];
+function strip(movieName){
+  return movieName.replace(/^(a |the |an )/i, '').trim();
+}
+
+
+
+const sortbenedict = Benedict.sort((a,b)=> strip(a)>strip(b)?1:-1);
+/*  if(strip(a)>strip(b)){
     return 1;
   }
   else {
     return -1;
-  }
-})
+  }*/
+document.querySelector('#Benedict').innerHTML =
+sortbenedict
+.map(ben => `<li>${ben}</li>`)
+.join('');
 console.log(sortbenedict);
